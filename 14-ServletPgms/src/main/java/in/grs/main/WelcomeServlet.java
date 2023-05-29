b51f74af9a6735e7be4f48907c3507a77e462755
gopals-777 <sharmagr2907@gmail.com>
2023-05-29 12:17:08 +0530

    first_commit1

--- 14-ServletPgms/src/main/java/in/grs/main/WelcomeServlet.java
+++ 14-ServletPgms/src/main/java/in/grs/main/WelcomeServlet.java
@@ -1,6 +1,7 @@
 package in.grs.main;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,31 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Welcome
  */
-@WebServlet("/Welcome")
+@WebServlet("/welcome")
 public class WelcomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public WelcomeServlet() {
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String parameter = request.getParameter("name");
+		PrintWriter out = response.getWriter();
+		out.println("<h1> Welcome "+parameter+"!</h1>");
 	}
 
 }
